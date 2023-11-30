e1875717239883dc584b2d098a112da4434fca0c
Michael Appiah <mappiahca@gmail.com>
2023-11-29 17:30:25 -0800

    Height update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return (1 + (left_height > right_height ? left_hight : right_hight));
+	return (height_l > height_r ? height_l : height_r);
 }
