5c0f0be462df958918a280cebbe5c9f9cf708d7a
Michael Appiah <mappiahca@gmail.com>
2023-11-29 18:27:20 -0800

    bug fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure
+ * max - Computes the maximum of two numbers
+ * @a: First number
+ * @b: Second number
  *
- * Return: Height of the tree, 0 if tree is NULL
+ * Return: Maximum of two numbers
+ */
+size_t max(size_t a, size_t b)
+{
+	return (a > b ? a : b);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of the tree, 0 if @tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (1 + max(binary_tree_height(tree->left),
+				binary_tree_height(tree->right)));
 }
